733d589e5ad4ac422e9b373f214bbed6be8e9ea2
Logan Arnold <Logan_Arnold@knights.ucf.edu>
2021-06-02 21:18:16 -0400

    [Fix] Slow down laser galvo movement

--- LaserShow/Laser.h
+++ LaserShow/Laser.h
@@ -11,16 +11,16 @@
 // defines the granularity of the line interpolation. 64 means that each line is split into steps of 64 pixels in the longer direction.
 // setting smaller values will slow down the rendering but will cause more linearity in the galvo movement,
 // setting bigger values will cause faster rendering, but lines will not be straight anymore.
-#define LASER_QUALITY 64
+#define LASER_QUALITY 128
 
 // Defines how long the galvos wait for the on/off toggling of the laser pointer (in microseconds), this will depend on your laser pointer.
 #define LASER_TOGGLE_DELAY 500
 // Defines how long the galvos wait at the end of a line (currently only used for the 3D cube rendering, in microseconds).
 #define LASER_LINE_END_DELAY 200
 // Defines the delay the laser waits after reaching a given position (in microseconds).
-#define LASER_END_DELAY 5
+#define LASER_END_DELAY 2000
 // Defines the delay after each laser movement (used when interpolating lines, in microseconds), if not defines, 0 is used
-//#define LASER_MOVE_DELAY 5
+//#define LASER_MOVE_DELAY 500
 
 // -- The following flags can be used to rotate/flip the output without changing the DAC wiring, just uncomment the desired swap/flip
 // define this to swap X and Y on the DAC
